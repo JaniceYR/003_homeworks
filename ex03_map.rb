5dc880c3756cf465e49ed595160fb3298ed708f5
Janice Yura Lee <janiceyuralee@gmail.com>
2017-03-30 22:50:27 -0700

    Add map class

---
+++ ex03_map.rb
@@ -0,0 +1,29 @@
+class Map
+
+  def initialize
+    @map =[]
+  end
+
+  def assign(key, value)
+    keys = @map.map { |ele| ele[0] }
+    @map << [key, value] unless keys.includes?(key)
+  end
+
+  def lookup(key)
+    keys = @map.map { |ele| ele[0] }
+    index = nil
+    keys.each_with_index do |k|
+      index = i if k == key
+    end
+    index.nil? nil : @map[index][1]
+  end
+
+  def remove(key)
+    @map.reject! { |ele| ele[0] == key}
+  end
+
+  def show
+    @map
+  end
+
+end
