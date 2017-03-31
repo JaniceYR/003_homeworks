cef3a0e50ee0f57b1f6d58f0ca61ff6285c07873
Janice Yura Lee <janiceyuralee@gmail.com>
2017-03-30 23:03:52 -0700

    Fix Error. includes? to be include? |k| to be |k, i|

--- ex03_map.rb
+++ ex03_map.rb
@@ -6,16 +6,16 @@ class Map
 
   def assign(key, value)
     keys = @map.map { |ele| ele[0] }
-    @map << [key, value] unless keys.includes?(key)
+    @map << [key, value] unless keys.include?(key)
   end
 
   def lookup(key)
     keys = @map.map { |ele| ele[0] }
     index = nil
-    keys.each_with_index do |k|
+    keys.each_with_index do |k, i|
       index = i if k == key
     end
-    index.nil? nil : @map[index][1]
+    index.nil? ? nil : @map[index][1]
   end
 
   def remove(key)
